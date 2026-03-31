18b8a3d1e81f7c7caa5a7da1a64add82815ceebc
Cristella <cristellatiara@gmail.com>
2026-03-31 20:14:00 +0200

    Uploaded the Javascript file
    This has the JavaScript code in order to create a more responsive, interactive website for users!

---
+++ script.js
@@ -0,0 +1,7 @@
+
+  alert("Welcome to my personal portfolio!");
+
+function showContact() 
+{ alert("Email: cristellatiara@gmail.com"); }
+
+document.getElementById("btn").addEventListener("click", showContact);
